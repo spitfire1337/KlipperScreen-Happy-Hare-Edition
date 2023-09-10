bb8ff4ee0efa69cce0184dab77e8ce40b7cd1525
Mike <skatermike21988@yahoo.com>
2023-09-10 14:35:11 -0400

    Update mmu_filaments.py

--- panels/mmu_filaments.py
+++ panels/mmu_filaments.py
@@ -204,7 +204,7 @@ class Panel(ScreenPanel):
         mmu = self._printer.get_stat("mmu")
         gate_status = mmu['gate_status']
         gate_material = mmu['gate_material']
-        gate_spoolmanid = mmu['gate_spoolmanid']
+        gate_spoolmanid = mmu['gate_spoolmanId']
         gate_color = mmu['gate_color']
         num_gates = len(gate_status)
 
